e2ebad95831eb9939bf5299b6a5a4a592ab0f161
Jose Nortes <nooors@gmail.com>
2021-11-22 17:31:30 +0100

    exercici 1 nivell 2 - Pantalla nom i model de nau

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import axios from "axios";
+
 Vue.use(Vuex);
+
+const resourceApi = "https://swapi.dev/api/starships";
 
 export default new Vuex.Store({
   state: {
     infoShips: null,
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setShips(state, ships) {
+      state.infoShips = ships;
+      console.log(state.infoShips);
+    },
+  },
+  actions: {
+    async getShips({ commit }) {
+      const response = await axios.get(resourceApi);
+      commit("setShips", response.data.results);
+      console.log(response.data.results);
+    },
+  },
   modules: {},
 });
--- src/views/Starships.vue
+++ src/views/Starships.vue
@@ -1,11 +1,61 @@
 <template>
-  <div class="list"></div>
+  <div class="list" v-if="listInfo">
+    <div class="list__items" v-for="ship in listInfo" :key="ship.name">
+      <div class="list__items__name">
+        {{ ship.name }}
+      </div>
+      <div class="list__items__model">
+        {{ ship.model }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import store from "@/store";
 
-export default {};
+export default {
+  beforeCreate() {
+    store.dispatch("getShips");
+  },
+  computed: {
+    listInfo: function () {
+      console.log(store.state.infoShips);
+      return store.state.infoShips;
+    },
+  },
+};
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.list
+  padding-bottom: 2rem
+  padding-top: 20%
+  margin: auto
+  width: 80%
+  @media(max-width: 1285px)
+    padding-top: 25%
+  @media(max-width: 1050px)
+    padding-top: 30%
+  @media(max-width: 850px)
+    padding-top: 35%
+  @media(max-width: 730px)
+    padding-top: 40%
+
+  .list__items
+    background-color: #000
+    margin: 1rem
+    border-radius: 0.5rem
+    box-shadow: 1px 1px 5px 2px #444242
+
+    .list__items__name, .list__items__model
+      color: hsl(0, 0%, 52%)
+      padding-left: 1.25rem
+      padding-top: .75rem
+
+    .list__items__name
+      text-transform: uppercase
+
+    .list__items__model
+      padding-bottom: .75rem
+</style>
