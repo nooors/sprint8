42a05fceea96a7792f864982e0fb64fef488ae08
Jose Nortes <nooors@gmail.com>
2021-12-11 12:18:47 +0100

    tests set

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+export default {
+  infoinputs: [
+    {
+      firstName: null,
+      firstNameError: false,
+      firstNameErrorMessage: false,
+    },
+    {
+      lastName: null,
+      lastNameError: false,
+      lastNameErrorMessage: false,
+    },
+    {
+      email: null,
+      emailError: false,
+      emailErrorMessage: false,
+    },
+    {
+      displayName: null,
+      diplayNameError: null,
+      displayNameErrorMessage: null,
+    },
+    {
+      password: null,
+      passwordError: false,
+      passwordErrorMessage: false,
+    },
+  ],
+};
